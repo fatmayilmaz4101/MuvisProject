0517d7f9ceca55f64697ea82413503e28e45d7ba
Fatma Yılmaz <79848362+fatmayilmsz@users.noreply.github.com>
2024-03-06 23:20:00 +0300

    Used AsyncStorage
    Added switch button.
    Added logout button.

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -2,9 +2,39 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../screens/Login/Login';
 import Home from '../screens/Home/Home';
-
+import {useNavigation} from '@react-navigation/native';
+import {Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useUser} from '../contexts/UserContext';
 const Stack = createStackNavigator();
+const LogoutButton = () => {
+  const {user, setUser} = useUser();
+  const navigation = useNavigation<any>();
+  const handleLogout = async () => {
+    const storedUserName = await AsyncStorage.getItem('username');
+    console.log('storagedan gelen username', storedUserName);
 
+    const storedPassword = await AsyncStorage.getItem('password');
+    console.log('storagedan gelen password', storedPassword);
+
+    const storedIsEnabled = await AsyncStorage.getItem('isEnabled');
+    console.log('storagedan gelen isEnabled', storedIsEnabled);
+
+    if (storedUserName && storedPassword) {
+      console.log('if e girdi');
+      setUser({userName: storedUserName, password: storedPassword});
+      console.log('storage username:', storedUserName);
+      console.log('storage password:', storedPassword);
+    } else {
+      setUser({userName: '', password: ''});
+      console.log('else e girdi');
+      console.log('user: ', user);
+    }
+
+    navigation.navigate('Login');
+  };
+  return <Button title="Çıkış Yap" onPress={handleLogout} />;
+};
 const Tabs: React.FC = () => {
   return (
     <Stack.Navigator
@@ -18,7 +48,10 @@ const Tabs: React.FC = () => {
         component={Login}
       />
       <Stack.Screen
-        options={{title: 'Ana Sayfa'}}
+        options={({route}) => ({
+          title: 'Ana Sayfa',
+          headerRight: () => route.name === 'Home' && <LogoutButton />,
+        })}
         name="Home"
         component={Home}
       />
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,8 +1,17 @@
 import React, {useState} from 'react';
-import {SafeAreaView, View, TextInput, Button, Image, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  TextInput,
+  Button,
+  Image,
+  Text,
+  Switch,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {useUser} from '../../contexts/UserContext';
 import {styles} from './login.styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = () => {
   const [userName, setUserName] = useState<string>('');
@@ -11,8 +20,25 @@ const Login = () => {
   const {setUser} = useUser();
   const {container, center, input, image, errorMessage} = styles;
   const navigation = useNavigation<any>();
+  const [isEnabled, setIsEnabled] = useState<boolean>(false);
+  const toggleSwitch = async (value: boolean) => {
+    setIsEnabled(value);
+    console.log(`${value}`);
+    if (value) {
+      try {
+        await AsyncStorage.setItem('username', userName);
+        await AsyncStorage.setItem('password', password);
+      } catch (error) {
+        console.error('Veri işlemleri hatası:', error);
+      }
+    } else {
+      await AsyncStorage.removeItem('username');
+      await AsyncStorage.removeItem('password');
+    }
+    await AsyncStorage.setItem('isEnabled', value.toString());
+  };
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     if (!userName || !password) {
       setError('Kullanıcı adı veya şifre boş bırakılamaz.');
       return;
@@ -25,7 +51,6 @@ const Login = () => {
 
     navigation.navigate('Home');
   };
-
   return (
     <SafeAreaView style={container}>
       <View style={center}>
@@ -38,16 +63,26 @@ const Login = () => {
           style={input}
           placeholderTextColor="gray"
           placeholder="Kullanıcı Adı"
-          onChangeText={text => setUserName(text)}
+          onChangeText={setUserName}
+          value={userName}
         />
         <TextInput
           style={input}
           placeholderTextColor="gray"
           placeholder="Şifre"
           secureTextEntry={true}
-          onChangeText={text => setPassword(text)}
+          onChangeText={setPassword}
+          value={password}
+        />
+        <Switch
+          trackColor={{false: '#767577', true: '#169D6B'}}
+          thumbColor={isEnabled ? 'white' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
         />
         <Text style={errorMessage}>{error}</Text>
+
         <Button color="#169D6B" title="Giriş Yap" onPress={handleLogin} />
       </View>
     </SafeAreaView>
